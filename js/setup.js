c7426f955e1984102b9c5955a1469c4335e933e3
Никита <nikitakhristov23@gmail.com>
2018-06-09 13:15:26 +0300

    Ошибки #1

--- js/setup.js
+++ js/setup.js
@@ -11,33 +11,28 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-var wizards = [
-  {
-    name: WIZARD_NAMES[i] + WIZARD_SURNAMES[i],
-    coatColor: COAT_COLOR[i],
-    eyesColor: EYES_COLOR[i]
-  },
-  {
-    name: WIZARD_NAMES[i] + WIZARD_SURNAMES[i],
-    coatColor: COAT_COLOR[i],
-    eyesColor: EYES_COLOR[i]
-  },
-  {
-    name: WIZARD_NAMES[i] + WIZARD_SURNAMES[i],
-    coatColor: COAT_COLOR[i],
-    eyesColor: EYES_COLOR[i]
-  },
-  {
-    name: WIZARD_NAMES[i] + WIZARD_SURNAMES[i],
-    coatColor: COAT_COLOR[i],
-    eyesColor: EYES_COLOR[i]
+var getWizardCharacter = function() {
+  for (var i = 0; i < 4; i++) {
+    var characterWizard = {
+      name: WIZARD_NAMES[getRandomNumber(0, WIZARD_NAMES.length)] + WIZARD_SURNAMES[getRandomNumber(0, WIZARD_SURNAMES.length )],
+      coatColor: COAT_COLOR[getRandomNumber(0, COAT_COLOR.length)],
+      eyesColor: EYES_COLOR[getRandomNumber(0, EYES_COLOR.length)]
+    }
+    wizards.push(characterWizard);
   }
-];
+}
 
 for (var i = 0; i < 4; i++) {
+  var wizards = [];
+  getWizardCharacter();
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
 }
+
